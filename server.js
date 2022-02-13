a2dbcc205564f578443d12c7f47205a34f154bc7
Gejix <geraldjuwah@gmail.com>
2022-02-13 23:48:20 +0100

    server.js updated

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require('body-parser');
+const mySecret = process.env['PW']
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 
@@ -20,5 +22,68 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ...`);
 });
+
+const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGO_URI);
+
+let url = 'mongodb+srv://Admin-Gerald:' + mySecret+'@helix.vzrxf.mongodb.net/FreeCodeDB'
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let urlSchema = new mongoose.Schema({
+  original: {type: String, required: true},
+  short: Number
+});
+
+let Url = mongoose.model('Url', urlSchema);
+
+let responseObject = {};
+
+app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }), (req, res) => {
+let inputUrl = req.body.url;
+
+let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+
+if(!inputUrl.match(urlRegex)){
+  res.json({error: 'Invalid URL'})
+  return
+}
+
+responseObject['original_url'] = inputUrl;
+
+let inputShort = 1
+
+Url.findOne({})
+    .sort({short: 'desc'})  
+    .exec((error, result) =>{
+      if (!error && result != undefined){
+        inputShort = result.short + 1;
+      }
+      if (!error){
+        Url.findOneAndUpdate(
+          {original: inputUrl},
+          {original: inputUrl, short: inputShort},
+          {new: true, upsert: true},
+          (error, savedUrl) => {
+            if(!error){
+              responseObject['short_url'] = savedUrl.short
+              res.json(responseObject);
+            }
+          }
+        );
+      }
+    });
+});
+
+app.get('/api/shorturl/:input', (req, res) => {
+  let input = req.params.input
+  Url.findOne({short: input}, (error, result) => {
+    if(!error && result != undefined){
+      res.redirect(result.original)
+    } else {
+      res.json('URL not found')
+    }
+  })
+})
